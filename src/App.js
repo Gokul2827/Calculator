773b29fabceee842f3da59f77af7a2d43a59bf6f
Gokul2827 <gokulakrishnana4@gmail.com>
2024-11-19 10:49:09 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [value,setValue]=useState('')
 
-export default App;
+ const click=(input)=>{
+  setValue(value+input)
+ }
+ const calculate=()=>{
+  try{
+    const val = eval(value)
+    setValue(val)
+  }catch{
+    setValue(value)
+  }
+ }  
+ const clear=()=>{
+  setValue('')
+ }
+  return (
+    <div>
+      <div className='content'>
+       {value || 0}
+      </div>
+      <div  className='btn'>
+        <a className='desbtn' onClick={clear}>Ac</a>
+        <a className='desbtn' onClick={()=>click('+')} >+/-</a>
+        <a className='desbtn' onClick={()=>click('%')}>%</a>
+        <a className='funbtn' onClick={()=>click('/')}>/</a>
+        </div>
+        <div  className='btn'>
+        <a onClick={()=>click('7')}>7</a>
+        <a onClick={()=>click('8')}>8</a>
+        <a onClick={()=>click('9')}>9</a>
+        <a className='funbtn' onClick={()=>click('*')}>*</a>
+        </div>
+        <div  className='btn'>
+        <a onClick={()=>click('4')}>4</a>
+        <a onClick={()=>click('5')}>5</a>
+        <a onClick={()=>click('6')}>6</a>
+        <a className='funbtn'  onClick={()=>click('-')}>-</a>
+        </div>
+        <div  className='btn'>
+        <a onClick={()=>click('1')}>1</a>
+        <a onClick={()=>click('2')}>2</a>
+        <a onClick={()=>click('3')}>3</a>
+        <a className='funbtn'  onClick={()=>click('+')}>+</a>
+     </div>
+     <div  className='btn'>
+        <a onClick={()=>click('0')}>0</a>
+        <a onClick={()=>click(',')}>,</a>
+        <a className='funbtn' onClick={calculate}>=</a>
+    </div>
+    </div>
+  )
+}
+export default App
